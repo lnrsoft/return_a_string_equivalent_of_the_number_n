6ee8ecf63515b30b95fc9d33513a7e00d811e40b
lnrsoft <lnrsoft@users.noreply.github.com>
2017-08-23 04:31:32 +0100

    minor changes

--- main.cpp
+++ main.cpp
@@ -8,19 +8,21 @@ int main(int argc, char* argv[])
 {
     QCoreApplication a(argc, argv);
     double x = 1.190;
-    double z = 6.6;
-    double y = 3.3;
+    double z = 6.634;
+    double y = 3.398;
     int precision;
-    double result = (x / z) * (0.009613 * y);
+    double result = (x / z) * (5.961 / y);
     QVariant(result).toString();
-    std::cout <<  "\nEnter the decimal places you want to round the result: ";
+    std::cout << "\nEnter the decimal places you want to round the result: ";
     std::cin >> precision;
     QString resultstring = QString::number(result, 'g', precision);
-    qDebug() << "1. the output is a double value.";
-    qDebug() << "The result by the compiler default setting: " << result << '\n';
-    qDebug() << "2. the output using QString::number static member function.";
-    qDebug() << "Setting the precision to " << precision << " the result: "
-             << qPrintable(resultstring) << '\n';
+    std::cout << "\n1.In this case the output is a double value." << std::endl;
+    std::cout << "The result by the compiler default setting is: " << result
+              << "\n" << std::endl;
+    std::cout << "2. Using the QString::number static member function."
+              << std::endl;
+    std::cout << "The result when we set the precision to " << precision << " is: "
+              << qPrintable(resultstring) << "\n" << std::endl;
     /*
      *  using qPrintable to remove qoutes from the output
     */
